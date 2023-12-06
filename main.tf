46e0e5581d8b1d5a1909b942ea57b369b57c904a
Fulto71 <109215509+Fulto71@users.noreply.github.com>
2023-12-06 15:16:37 +0000

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,69 @@
+variable "cloudinit_template_name" {
+    type = string 
+}
+
+variable "proxmox_node" {
+    type = string
+}
+
+variable "ssh_key" {
+  type = string 
+  sensitive = true
+}
+
+resource "proxmox_vm_qemu" "k8s-4" {
+  count = 1
+  name = "k8s-14${count.index + 1}"
+  target_node = var.proxmox_node
+  clone = var.cloudinit_template_name
+  agent = 1
+  os_type = "cloud-init"
+  cores = 1
+  sockets = 1
+  cpu = "host"
+  memory = 2048
+  scsihw = "virtio-scsi-pci"
+  bootdisk = "scsi0"
+
+  disk {
+    slot = 0
+    size = "80G"
+    type = "scsi"
+    storage = "local-zfs"
+  }
+
+  network {
+    model = "virtio"
+    bridge = "vmbr0"
+  }
+  
+  lifecycle {
+    ignore_changes = [
+      network,
+    ]
+  }
+
+  ipconfig0 = "ip=172.20.0.20${count.index + 1}/24,gw=172.20.0.1"
+  nameserver = "172.20.0.31"
+  
+  sshkeys = <<EOF
+  ${var.ssh_key}
+  EOF
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ terraform.tfvars
@@ -0,0 +1,13 @@
+#pm_api_url = "https://192.168.8.131:8006/api2/json"
+#pm_api_token_id = "root@pam!form"  # API Token ID
+#pm_api_token_secret = "63e9fe84-62ef-412c-999a-83d91343d4a2"
+#cloudinit_template_name = "conteste"
+#proxmox_node = "pve"
+#ssh_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMOa/XdiXbOKhaCejTQ9fcdGrf/Zw3S25REZdIj8CXcl your_email@example.com"
+
+pm_api_url = "https://10.10.103.2:8006/api2/json"
+pm_api_token_id = "josue.digbeu@pve!test1"  # API Token ID
+pm_api_token_secret = "07518e30-235d-4829-957a-86ff1a5d0072"
+cloudinit_template_name = "tem-vm"
+proxmox_node = "ve0"
+ssh_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMOa/XdiXbOKhaCejTQ9fcdGrf/Zw3S25REZdIj8CXcl your_email@example.com"
